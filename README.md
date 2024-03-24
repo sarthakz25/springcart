41a8c52f2a89539853669c47c1cff26501a03d13
Sarthak Khandelwal <sarthakhandelwal@gmail.com>
2024-03-24 22:13:51 +0530

    service discovery and restaurant service init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Food Delivery System
+## Microservices and Architecture Project
---
+++ restaurant-service/src/main/java/com/fooddeliverysystem/restaurantservice/config/RestaurantKafkaConfig.java
@@ -0,0 +1,10 @@
+package com.fooddeliverysystem.restaurantservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class RestaurantKafkaConfig {
+
+}
