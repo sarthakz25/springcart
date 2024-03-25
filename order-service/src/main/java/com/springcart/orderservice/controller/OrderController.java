01371c469ca3568fc52a985345f6ab8606cbe29a
Sarthak Khandelwal <sarthakhandelwal@gmail.com>
2024-03-25 23:45:33 +0530

    add order, product service

---
+++ order-service/src/main/java/com/springcart/orderservice/controller/OrderController.java
@@ -0,0 +1,23 @@
+package com.springcart.orderservice.controller;
+
+import com.springcart.orderservice.dto.OrderRequest;
+import com.springcart.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/order")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        orderService.placeOrder(orderRequest);
+
+        return "Order placed successfully";
+    }
+}
---
+++ product-service/src/test/java/com/springcart/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,65 @@
+package com.springcart.productservice;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.springcart.productservice.dto.ProductRequest;
+import com.springcart.productservice.repository.ProductRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ProductServiceApplicationTests {
+
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper mapper;
+    @Autowired
+    private ProductRepository repository;
+
+    static {
+        mongoDBContainer.start();
+    }
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+        dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
+
+    @Test
+    void shouldCreateProduct() throws Exception {
+        ProductRequest productRequest = getProductRequest();
+        String productRequestString = mapper.writeValueAsString(productRequest);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(productRequestString))
+                .andExpect(status().isCreated());
+
+        Assertions.assertEquals(1, repository.findAll().size());
+    }
+
+    private ProductRequest getProductRequest() {
+        return ProductRequest.builder()
+                .name("Samsung Galaxy S24 Ultra")
+                .description("A flagship smartphone featuring cutting-edge technology, powerful performance, and an advanced camera system.")
+                .price(BigDecimal.valueOf(129999))
+                .build();
+    }
+}
